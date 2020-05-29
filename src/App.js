a6d63c2f21337439cc9bea0842e91c05b0e981a9
Allohamora <herman2000@yandex.ru>
2020-05-29 15:35:53 +0300

    Added styled and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Switch, Route, Redirect, BrowserRouter, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`  
+  display: flex;
+  justify-content: space-around;
+
+  color: red;
+`;
+
+const Navbar = props => (
+  <Container>
+    <Link to="/" >Home</Link>
+    <Link to="/test" >Test</Link>
+  </Container>
+);
+
+const Home = props => (<div>Home</div>);
+const Test = props => (<div>Test</div>);
 
 function App() {
   return (
-    <div>Test</div>
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/test" render={Test} />
+        <Redirect to="/" />
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
