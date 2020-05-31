3905339c678cce558f511bca67e232617bbeebbe
Дмитрий <burydima28122003@gmail.com>
2020-05-31 08:24:10 +0300

    Start page Home

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+    width: 100%;
+    height: 125px;
+
+    display: flex;
+    flex-direction: column;
+
+    background: #CECCCE;
+    transition: .3s;
+
+    padding: 25px;
+`;
+
+export const Note = (props) => {
+
+    return (
+        <Wrap>
+            
+        </Wrap>
+    );
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { Page } from 'components/Page';
+import { Note } from 'components/Note/';
+import { Input } from 'components/Input';
+import { Button } from 'components/Button';
+
+const HomeInput = styled(Input)`
+    width: 100%;
+    box-sizing: border-box;
+`
+const RadioWrap = styled.div`
+    width: 100%;
+    margin: 0;
+
+    display: flex;
+    justify-content: space-between;    
+`
 
 export const Home = props => {
 
     return (
         <Page>
-            Home
+            <HomeInput type="text" placeholder="Поиск..." />
+                <RadioWrap>
+                    <Button>Хочу помочь</Button>
+                    <Button>Нужна помощь</Button>
+                </RadioWrap>
+            <Note />
         </Page>
     );
 };
